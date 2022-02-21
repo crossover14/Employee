a2f2e81e4a128e20d8af25bb4053b0f39268e297
Leandro Vieira <leandro.v.pordeus@gmail.com>
2022-02-20 22:36:44 -0300

    Iniciando o projeto

---
+++ ExercEmplyer/Entities/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercEmplyer.Entities
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public int Hours { get; set; }
+
+    }
+}
---
+++ ExercEmplyer/Entities/OutsourceEmployee.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercEmplyer.Entities
+{
+    class OutsourceEmployee
+    {
+    }
+}
